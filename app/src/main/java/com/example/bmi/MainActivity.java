647f7a05b05244a2735704567dba1f7dd73f5b7d
ashin paul <ashinpaul46@gmail.com>
2023-07-07 09:24:40 +0530

    first commit

---
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.bmi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    EditText e1, e2, e3;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.e1);
+        e2 = (EditText) findViewById(R.id.e2);
+        e3 = (EditText) findViewById(R.id.e3);
+        b = (Button) findViewById(R.id.b1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                float w = Float.parseFloat(String.valueOf(e1.getText()));
+                float h = Float.parseFloat(String.valueOf(e2.getText())) / 100;
+                float bmi = w / (h * h);
+                e3.setText(String.valueOf(bmi));
+            }
+        });
+    }
+}
